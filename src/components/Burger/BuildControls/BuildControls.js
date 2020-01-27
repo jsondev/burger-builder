1835784393185d93ac0290c0ef3da4cd051b9524
jsondev <jsondev1.0@outlook.com>
2020-01-27 09:24:33 -0500

    added disable control to the less button

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -17,7 +17,7 @@ const BuildControls = props => (
         key={ctrl.label}
         label={ctrl.label}
         added={()=> props.ingredientAdded(ctrl.type)}
-        removed={()=> props.ingredientRemoved(ctrl.type)}
+        removed={()=> props.ingredientRemoved(ctrl.type)} disabled={props.disabled[ctrl.type]}
       />
     ))}
   </div>
