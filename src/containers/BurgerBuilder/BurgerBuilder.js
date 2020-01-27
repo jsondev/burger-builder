95e3bf79a9f1d6fd29d16891c3210997f8cef936
jsondev <jsondev1.0@outlook.com>
2020-01-27 09:17:48 -0500

    added remove control

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -22,7 +22,26 @@ class BurgerBuilder extends Component {
     totalPrice: 4
   };
 
-  removeIngredientHandler = type => {};
+  removeIngredientHandler = type => {
+        //  Get the value of the ingredient
+        const oldCount = this.state.ingredients[type];
+        //  Update the ingredient current value
+        const updatedCounted = oldCount - 1;
+        //  Create a new state object from the current state
+        const updatedIngredients = {
+          ...this.state.ingredients
+        };
+        //  Update the new state object with the new ingredient count number
+        updatedIngredients[type] = updatedCounted;
+        //  Get the current price of the ingredient
+        const priceDeduction = INGREDIENT_PRICES[type];
+        //  Get current total for the entire burger
+        const oldPrice = this.state.totalPrice;
+        //  Create the new total from the ingredient price + total burger
+        const newPrice = oldPrice - priceDeduction;
+        //  Update the state with the new ingredient state and total price
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+  };
 
   addIngredientHandler = type => {
     //  Get the value of the ingredient
@@ -49,7 +68,7 @@ class BurgerBuilder extends Component {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls ingredientAdded={this.addIngredientHandler}/>
+        <BuildControls ingredientAdded={this.addIngredientHandler} ingredientRemoved={this.removeIngredientHandler}/>        
       </Aux>
     );
   }
