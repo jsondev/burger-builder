ff4c0e8bebbb8ac314ea617027f7e9ed65f5c500
jsondev <jsondev1.0@outlook.com>
2020-01-27 09:33:11 -0500

    added total price to UI

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,6 +11,7 @@ const controls = [
 
 const BuildControls = props => (
   <div className={classes.BuildControls}>
+    <p>Current Price: <strong>${props.price.toFixed(2)}</strong></p>
     {/* Loop through the controls */}
     {controls.map(ctrl => (
       <BuildControl
