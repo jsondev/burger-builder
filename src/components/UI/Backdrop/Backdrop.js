1b5efac11a0040c2b5bb500c2c023b434834e907
jsondev <jsondev1.0@outlook.com>
2020-01-29 18:53:36 +0000

    Test commit

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -2,6 +2,6 @@ import React from "react";
 import classes from "./Backdrop.module.css";
 
 const Backdrop = props =>
-  props.show ? <div className={classes.Backdrop}></div> : null;
+  props.show ? <div className={classes.Backdrop}></div> : null; 
 
 export default Backdrop;
