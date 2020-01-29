11ca47b936d082b84e66f10411f932d975f8b429
jsondev <jsondev1.0@outlook.com>
2020-01-29 15:59:02 -0500

    init the toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Aux from "../../hoc/Auxiliary";
 import classes from "./Layout.module.css";
+import Toolbar from "../Navigation/Toolbar";
 
 const Layout = props => (
   <Aux>
-    <div>Toolbar, Sidedrawer, Backdroup</div>
+    <div>
+      <Toolbar />
+      Sidedrawer, Backdroup</div>
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
