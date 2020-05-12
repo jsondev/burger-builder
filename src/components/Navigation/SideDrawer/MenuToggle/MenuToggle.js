5571d450f75a28e7e3b63f92901588a8f6445904
jsondev <jsondev1.0@outlook.com>
2020-05-12 15:50:08 -0400

    added side drawer and the ability to open and close

---
+++ src/components/Navigation/SideDrawer/MenuToggle/MenuToggle.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MenuToggle = props => (
+    <div onClick={props.clicked}>MENU</div>
+);
+
+export default MenuToggle;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,11 @@ import React from "react";
 import classes from "./Toolbar.module.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import MenuToggle from "../SideDrawer/MenuToggle/MenuToggle";
 
 const Toolbar = props => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
+    <MenuToggle clicked={props.open} />
     <div className={classes.Logo}>
       <Logo />
     </div>
